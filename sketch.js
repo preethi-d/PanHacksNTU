8deca1bad041063760799b76161bd3162ca61755
preethid <e0203237@u.nus.edu>
2019-03-23 21:32:36 +0800

    sounds correspond to keys

--- sketch.js
+++ sketch.js
@@ -1,33 +1,76 @@
 var song;
-var button;
-var hihat = '/sounds/hihat.wav';
-var boom = '/sounds/boom.wav';
-var clap = '/sounds/clap.wav';
-var kick = '/sounds/kick.wav';
-var openhat = '/sounds/openhat.wav';
-var ride = '/sounds/ride.wav';
-var snare = '/sounds/snare.wav';
-var tink = '/sounds/tink.wav';
-var tom = '/sounds/tom.wav';
+var hihatButton;
+var hihat;
+var boomButton;
+var boom;
+var clapButton;
+var clap;
+var kickButton;
+var kick;
+var openhatButton;
+var openhat;
+var rideButton;
+var ride;
+var snareButton;
+var snare;
+var tinkButton;
+var tink;
+var tomButton;
+var tom;
 
 function preload() {
   soundFormats('mp3', 'wav');
-  song = loadSound(snare);
+  snare = loadSound('/sounds/snare.wav');
+  hihat = loadSound('/sounds/hihat.wav');
+  boom = loadSound('/sounds/boom.wav');
+  clap = loadSound('/sounds/clap.wav');
+  kick = loadSound('/sounds/kick.wav');
+  openhat = loadSound('/sounds/openhat.wav');
+  ride = loadSound('/sounds/ride.wav');
+  tink = loadSound('/sounds/tink.wav');
+  tom = loadSound('/sounds/tom.wav');
 
 }
 
 function setup() {
-  // put setup code here
-  createCanvas(200,200);
-  song.setVolume(10);
-  song.play();
-  //button = createButton("play");
-  //button.mousePressed(togglePlay);
-  //background(51);
+
 }
 
 function draw() {
   // put drawing code here
-  background('red');
+  background(0);
+}
 
+function keyPressed() {
+  console.log(key.toLowerCase());
+  switch (key.toLowerCase()) {
+    case "d":
+      hihat.play();
+      break;
+    case "f":
+      boom.play();
+      break;
+    case "g":
+      clap.play();
+      break;
+    case "h":
+      kick.play();
+      break;
+    case "j":
+      openhat.play();
+      break;
+    case "c":
+      ride.play();
+      break;
+    case "v":
+      snare.play();
+      break;
+    case "b":
+      tink.play();
+      break;
+    case "n":
+      tom.play();
+      break;
+
+  }
 }
