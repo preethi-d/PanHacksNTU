296e6573c2a420e2f647657a28ddbaf3758c6776
yogtew <reachyogusvi@gmail.com>
2019-03-23 23:42:12 +0800

    Add circles

--- sketch.js
+++ sketch.js
@@ -30,46 +30,37 @@ function setup() {
 
   createCanvas(1430,690);
   bg = loadImage('assets/drum.jpg');
-  
-  // put setup code here
-  song.setVolume(10);
-  song.play();
-  //button = createButton("play");
-  //button.mousePressed(togglePlay);
-  //background(51);
-  
-  // space out the buttons along the window
-  button1 = createButton('sound');
-  button1.position(10, 200);
-
-  button2 = createButton('sound');
-  button2.position(360, 160);
-
-  button3 = createButton('sound');
-  button3.position(715, 120);
-
-  button4 = createButton('sound');
-  button4.position(1065, 160);
-
-  button5 = createButton('sound');
-  button5.position(1380, 200);
-
-  button6 = createButton('sound');
-  button6.position(185, 440);
-
-  button7 = createButton('sound');
-  button7.position(536, 400);
-
-  button8 = createButton('sound');
-  button8.position(880, 400);
-
-  button9 = createButton('sound');
-  button9.position(1220, 440);
-
+}
   // use the next line to link what happens when u press the button
   // button.mousePressed();
 
 function draw() {
   background(bg);
-  // display all the buttons
+
+  fill('white');
+  ellipse(50, 200, 80, 80);
+
+  fill('white');
+  ellipse(360, 160, 80, 80);
+
+  fill('white');
+  ellipse(715, 120, 80, 80);
+
+  fill('white');
+  ellipse(1065, 160, 80, 80);
+
+  fill('white');
+  ellipse(1380, 200, 80, 80);
+
+  fill('white');
+  ellipse(185, 440, 80, 80);
+
+  fill('white');
+  ellipse(536, 400, 80, 80);
+
+  fill('white');
+  ellipse(880, 400, 80, 80);
+
+  fill('white');
+  ellipse(1220, 440, 80, 80);
 }
